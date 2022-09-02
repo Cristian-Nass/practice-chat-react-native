6fc79dd226f5a6617a4d8bfb22391b9373eda6d6
Cristian <cristian.tirdad@gmail.com>
2022-09-02 12:00:37 +0200

    Implemented react navigation

--- App.tsx
+++ App.tsx
@@ -1,4 +1,16 @@
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <Login />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
